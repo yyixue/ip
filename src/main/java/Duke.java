174a88ddb9503c86d1c5935cf53e425577a80af2
yyixue <yanyixue99@gmail.com>
2021-01-20 10:28:09 +0800

    Level-0: Greet

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,13 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println("----------------------------------------------------");
+        System.out.println(logo);
+        System.out.println("Aloha! I'm Duke");
+        System.out.println("What can I do for you today?");
+        System.out.println("----------------------------------------------------");
+        System.out.println("Bye! Hope you have a wonderful day.");
+        System.out.println("----------------------------------------------------");
+
     }
 }
