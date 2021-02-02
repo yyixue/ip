a8a12da098beb6f8b6c554f5cc58c43c06d9a1f2
yyixue <yanyixue99@gmail.com>
2021-02-02 17:36:33 +0800

    A-TextUiTesting

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -13,7 +13,31 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Duke.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Task.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Todo.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Event.java
+then
+    echo "********** BUILD FAILURE **********"
+    exit 1
+fi
+
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Deadline.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
